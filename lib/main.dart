a4a36aae1fc485728c8c638f036f440e05fce4e6
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-04-22 22:06:08 +0530

    Title has changed.

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ void main() {
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title:Center(
-            child: Text('Hello there, Get ready to die !')) ,   
+            child: Text('Hello there, Miss me?')) ,   
           backgroundColor: Colors.blueGrey[900],
         ),   
         body: Center(

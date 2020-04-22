df4e4deb075e1ddbb07194655e481eb9e6fba3cc
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-04-22 21:23:48 +0530

    Flutter test app created.

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+//main is the starting point
+/*
+void main() => runApp(
+      MaterialApp(
+        home: Center(
+          child: Text('Hello Vidu'),
+        ),
+      ),
+    );
+*/
+
+void main() { 
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title:Center(
+            child: Text('Hello there !')) ,   
+          backgroundColor: Colors.blueGrey[900],
+        ),   
+        body: Center(
+          child: Image(
+            //image: NetworkImage('https://media.giphy.com/media/26tPo9rksWnfPo4HS/giphy.gif'),
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),       
+      ),
+    ),  
+  );
+}
